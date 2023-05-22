5ee0af84b6bee760f5791c9bd7516960e092b760
Chris <c.r.zambito@gmail.com>
2023-05-22 09:26:45 -0400

    fixing native, added png to home and footer, made 2 templates for voting

--- ghi/src/Footer.js
+++ ghi/src/Footer.js
@@ -1,10 +1,16 @@
 import React from "react";
+import heroImage from "./static/hero-ithink.png";
 
 function Footer() {
     return (
         <footer className="w-full bg-gray-800 py-4">
-        <div className="container mx-auto text-center text-white">
+        <div className="container mx-auto flex justify-between items-center px-4 md:px-0 text-white">
+            <div className="w-full text-center">
             <p>Made with ❤️ by The Binary Bandits</p>
+            </div>
+            <div className="flex justify-end">
+            <img src={heroImage} alt="iThink" className="w-16 h-16" />
+            </div>
         </div>
         </footer>
     );
