1f480bc9d7dbb5bb3159f54799839de5a3857e80
smalhadid <smalhadid@gmail.com>
2023-05-30 16:11:09 -0400

    added list images function

--- project_iThink/routers/image.py
+++ project_iThink/routers/image.py
@@ -10,7 +10,11 @@ from queries.image import ImageQueries
 
 router = APIRouter()
 
-@router.post("/images/")
+@router.post("/api/images/")
 async def create_upload_image(image: UploadFile = File(...), image_queries: ImageQueries = Depends()):
     image_id = await image_queries.save_image(image)
     return {"image_id": str(image_id)}
+
+@router.get("/api/images/list/")
+async def get_all_projects(repo: ImageQueries = Depends()):
+    return repo.get_all_images()
