50dd5049c374991a892fa34611d95c5c9b820e6f
Chris <c.r.zambito@gmail.com>
2023-06-05 18:32:22 -0400

    updated createProject to show the title and description once added to a banner

--- ghi/src/CreateProject.js
+++ ghi/src/CreateProject.js
@@ -7,38 +7,45 @@ function CreateProject() {
     const [description, setDescription] = useState("");
     const [isLoading, setIsLoading] = useState(false);
     const [isSuccess, setIsSuccess] = useState(false);
+    const [successTitle, setSuccessTitle] = useState("");
+    const [successDescription, setSuccessDescription] = useState("");
 
     const handleSubmit = async (event) => {
-        event.preventDefault();
-        setIsLoading(true);
+    event.preventDefault();
+    setIsLoading(true);
 
-        const token = localStorage.getItem("token"); // Retrieve the JWT token from local storage
-        const response = await fetch("http://localhost:8000/api/projects", {
+    const projectTitle = title; // store the title
+    const projectDescription = description; // store the description
+
+    const token = localStorage.getItem("token"); // Retrieve the JWT token from local storage
+    const response = await fetch("http://localhost:8000/api/projects", {
         method: "POST",
         headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`, // Add the JWT token to the headers
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`, // Add the JWT token to the headers
         },
         body: JSON.stringify({
-            title,
-            description,
+        title,
+        description,
         }),
-        });
+    });
 
-        const project = await response.json();
+    const project = await response.json();
 
-        console.log(project);
+    console.log(project);
 
-        setIsLoading(false);
-        setTitle("");
-        setDescription("");
-        setIsSuccess(true);
-        toast.success("New project was submitted successfully! 😺");
+    setIsLoading(false);
+    setTitle("");
+    setDescription("");
+    setIsSuccess(true);
+    setSuccessTitle(projectTitle);
+    setSuccessDescription(projectDescription);
+    toast.success("New project was submitted successfully! 😺");
     };
 
     return (
         <div className="bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 min-h-screen flex items-center justify-center">
-            <ToastContainer />
+        <ToastContainer />
         <div className="w-full max-w-md bg-white rounded-lg shadow-lg p-8">
             <h1 className="text-2xl font-bold mb-4">Create Project</h1>
             <form onSubmit={handleSubmit}>
@@ -85,19 +92,123 @@ function CreateProject() {
                 </button>
             </div>
             </form>
-            {isSuccess && (
-            <div
-                className="mt-4 bg-green-100 border-l-4 border-green-500 text-green-700 p-4"
-                role="alert"
-            >
-                <p className="font-bold">Project created successfully!</p>
-                <p>Title: {title}</p>
-                <p>Description: {description}</p>
-            </div>
-            )}
+                {isSuccess && (
+                <div
+                    className="mt-4 bg-green-100 border-l-4 border-green-500 text-green-700 p-4"
+                    role="alert"
+                >
+                    <p className="font-bold">Project created successfully!</p>
+                    <p>Title: {successTitle}</p>
+                    <p>Description: {successDescription}</p>
+                </div>
+                )}
         </div>
         </div>
     );
 }
 
 export default CreateProject;
+
+// import { useState } from "react";
+// import { ToastContainer, toast } from "react-toastify";
+// import "react-toastify/dist/ReactToastify.css";
+
+// function CreateProject() {
+//     const [title, setTitle] = useState("");
+//     const [description, setDescription] = useState("");
+//     const [isLoading, setIsLoading] = useState(false);
+//     const [isSuccess, setIsSuccess] = useState(false);
+
+//     const handleSubmit = async (event) => {
+//         event.preventDefault();
+//         setIsLoading(true);
+
+//         const token = localStorage.getItem("token"); // Retrieve the JWT token from local storage
+//         const response = await fetch("http://localhost:8000/api/projects", {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json",
+//             Authorization: `Bearer ${token}`, // Add the JWT token to the headers
+//         },
+//         body: JSON.stringify({
+//             title,
+//             description,
+//         }),
+//         });
+
+//         const project = await response.json();
+
+//         console.log(project);
+
+//         setIsLoading(false);
+//         setTitle("");
+//         setDescription("");
+//         setIsSuccess(true);
+//         toast.success("New project was submitted successfully! 😺");
+//     };
+
+//     return (
+//         <div className="bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 min-h-screen flex items-center justify-center">
+//             <ToastContainer />
+//         <div className="w-full max-w-md bg-white rounded-lg shadow-lg p-8">
+//             <h1 className="text-2xl font-bold mb-4">Create Project</h1>
+//             <form onSubmit={handleSubmit}>
+//             <div className="mb-4">
+//                 <label
+//                 className="block text-gray-700 font-bold mb-2"
+//                 htmlFor="title"
+//                 >
+//                 Title
+//                 </label>
+//                 <input
+//                 className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+//                 id="title"
+//                 type="text"
+//                 placeholder="Enter project title"
+//                 value={title}
+//                 onChange={(event) => setTitle(event.target.value)}
+//                 />
+//             </div>
+//             <div className="mb-4">
+//                 <label
+//                 className="block text-gray-700 font-bold mb-2"
+//                 htmlFor="description"
+//                 >
+//                 Description
+//                 </label>
+//                 <textarea
+//                 className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+//                 id="description"
+//                 placeholder="Enter project description"
+//                 value={description}
+//                 onChange={(event) => setDescription(event.target.value)}
+//                 ></textarea>
+//             </div>
+//             <div className="flex items-center justify-between">
+//                 <button
+//                 className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline ${
+//                     isLoading && "opacity-50 cursor-wait"
+//                 }`}
+//                 type="submit"
+//                 disabled={isLoading}
+//                 >
+//                 {isLoading ? "Creating..." : "Create"}
+//                 </button>
+//             </div>
+//             </form>
+//             {isSuccess && (
+//             <div
+//                 className="mt-4 bg-green-100 border-l-4 border-green-500 text-green-700 p-4"
+//                 role="alert"
+//             >
+//                 <p className="font-bold">Project created successfully!</p>
+//                 <p>Title: {title}</p>
+//                 <p>Description: {description}</p>
+//             </div>
+//             )}
+//         </div>
+//         </div>
+//     );
+// }
+
+// export default CreateProject;
