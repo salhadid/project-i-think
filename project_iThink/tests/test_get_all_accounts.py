fc00fc4e4b4bb8087f28a8527504045e82eb8f98
smalhadid <smalhadid@gmail.com>
2023-06-06 14:08:00 -0400

    added Chris unit test

--- project_iThink/tests/test_get_all_accounts.py
+++ project_iThink/tests/test_get_all_accounts.py
@@ -1,6 +1,7 @@
 from fastapi.testclient import TestClient
 from main import app
 from queries.accounts import AccountQueries
+from queries.projects import ProjectQueries
 
 client = TestClient(app=app)
 
@@ -11,6 +12,18 @@ class AccountQueriesMock():
 def test_get_all_accounts():
     app.dependency_overrides[AccountQueries] = AccountQueriesMock
     response = client.get("/api/accounts/")
+    assert response.status_code == 200
+    assert response.json() == []
+    app.dependency_overrides = {}
+
+
+class ProjectQueriesMock():
+    def get_all_projects(self):
+        return []
+
+def test_get_all_projects():
+    app.dependency_overrides[ProjectQueries] = ProjectQueriesMock
+    response = client.get("/api/projects/list/")
     assert response.status_code == 200
     assert response.json() == []
     app.dependency_overrides = {}
