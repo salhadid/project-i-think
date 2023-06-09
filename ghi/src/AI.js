cc685eca28f0564e9fa44b54470104fa1e6748c3
smalhadid <smalhadid@gmail.com>
2023-06-09 11:45:27 -0400

    fixed broken link on main

--- ghi/src/AI.js
+++ ghi/src/AI.js
@@ -362,7 +362,7 @@ function AI() {
                                         onClick={handleSubmitYes}
                                         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
                                     >
-                                        SUBMIT For Voting
+                                        SUBMIT
                                     </button>
                                 </div>
                             </div>
