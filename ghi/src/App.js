f0d2cd118de86b1b04b396311e867ae1f27d9b18
smalhadid <smalhadid@gmail.com>
2023-10-23 13:11:38 -0400

    hopefully last test

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import LoginForm from "./LoginForm.js";
 import AddUserForm from "./AddUserForm.js";
 import AI from "./AI";
@@ -20,11 +20,60 @@ import Faq from "./faq";
 
 function App() {
     const [loggedIn, setLoggedIn] = useState(false);
+    const location = useLocation();
 
     useEffect(() => {
         const token = localStorage.getItem("token");
         setLoggedIn(token !== null);
     }, []);
+
+    useEffect(() => {
+        switch (location.pathname) {
+            case "/login":
+                document.title = "Login to App";
+                break;
+            case "/register":
+                document.title = "Register for App";
+                break;
+            case "/ai":
+                document.title = "AI Interface";
+                break;
+            case "/update":
+                document.title = "Update User Info";
+                break;
+            case "/create/project":
+                document.title = "Create a Project";
+                break;
+            case "/projectForm":
+                document.title = "Project Form";
+                break;
+            case "/VisionBoard":
+                document.title = "Vision Board";
+                break;
+            case "/ImageList":
+                document.title = "Image List";
+                break;
+            case "/editResponses":
+                document.title = "Edit Responses";
+                break;
+            case "/contact":
+                document.title = "Contact Us";
+                break;
+            case "/pricing":
+                document.title = "Pricing Details";
+                break;
+            case "/faq":
+                document.title = "Frequently Asked Questions";
+                break;
+            case "/":
+                document.title = loggedIn ? "Home" : "iThink";
+                break;
+            default:
+                document.title = "Your Default Title";
+                break;
+        }
+    }, [location.pathname, loggedIn]);
+
     const domain = /https:\/\/[^/]+/;
     const basename = process.env.PUBLIC_URL.replace(domain, "");
     return (
